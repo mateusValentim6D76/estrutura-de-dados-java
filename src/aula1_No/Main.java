7de12b917f84054e3b6334bc8ee2d051f6bb8576
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-09-18 00:46:14 -0300

    Refatoração da classe No

--- src/aula1_No/Main.java
+++ src/aula1_No/Main.java
@@ -4,15 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		No no1 = new No("Conteudo no1");
+		/*
+		 * Declarar o tipo do Objeto a ser recebido 
+		 * dentro da instanciação do objeto é inferente
+		 */
 		
-		No no2 = new No("Conteudo no1");
+		No<String> no1 = new No<>("Conteudo no1");		
+		No<String> no2 = new No<String>("Conteudo no2");
 		no1.setProximoNo(no2);
 
-		
-		No<String> n3 = new No("Conteudo no3");
-		No n4 = new No<>("Conteudo no4");
-		
+		No<String> no3 = new No<>("Conteudo no3");
+		no2.setProximoNo(no3);		
+		No<String> no4 = new No<>("Conteudo no4");
+		no3.setProximoNo(no4);
 		  
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
 	}
 }
