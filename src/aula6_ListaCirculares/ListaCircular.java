f5bf615766d2d6891ca547934eefd62a0e29c222
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-10-04 15:01:08 -0300

    Implementação do método getNo e get

--- src/aula6_ListaCirculares/ListaCircular.java
+++ src/aula6_ListaCirculares/ListaCircular.java
@@ -6,6 +6,24 @@ public class ListaCircular<T> {
 	private No<T> cauda;
 	private int tamanhoLista;
 
+	public T get(int index) {
+		return this.getNo(index).getConteudo();
+	}
+	
+	private No<T> getNo(int index){
+		if (this.isEmpty())
+			throw new IndexOutOfBoundsException("A lista está vazia!!!");
+		if(index == 0) {
+			return this.cauda;
+		}
+		No<T> noAuxiliar = this.cauda;
+		for(int i = 0; (i < index) && (noAuxiliar != null); i++) {
+				noAuxiliar = noAuxiliar.getNoProximo();
+		}
+		
+		return noAuxiliar;
+	}
+	
 	public boolean isEmpty() {
 		return tamanhoLista == 0 ? true : false;
 	}
