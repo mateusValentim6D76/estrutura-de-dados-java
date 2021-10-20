cb657d6a1ba7b86b45c6004c78d3491130fe7a68
Mateus Valentim de Oliveira <mateus.oliveira@keeggo.com>
2021-10-19 22:13:19 -0300

    Primeira parte da implementação do método Remover - Passando o conteúdo e verificando se a raiz é nula

--- src/aula7_Arvores/ArvoreBinaria.java
+++ src/aula7_Arvores/ArvoreBinaria.java
@@ -2,59 +2,83 @@ package aula7_Arvores;
 
 public class ArvoreBinaria<T extends Comparable<T>> {
 
-	private BinNo<T> raiz;
-	
-	public ArvoreBinaria() {
-		this.raiz = null;
-	}
-	
-	private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
-		if(atual == null) {
-			return novoNo;
-		} else if(novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
-			atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
-		} else {
-			atual.setNoDir(inserir(atual.getNoDir(), novoNo));
-		}
-		return atual;
-	}
+    private BinNo<T> raiz;
 
-	public void exibirInOrdem(){
-		System.out.println("\n exibir em InOrdem");
-		exibirInOrdem(this.raiz);
-	}
+    public ArvoreBinaria() {
+        this.raiz = null;
+    }
 
-	private void exibirInOrdem(BinNo<T> atual) {
-		if (atual != null) {
-			exibirInOrdem(atual.getNoEsq());
-			System.out.println(atual.getConteudo() + ", ");
-			exibirInOrdem(atual.getNoDir());
-		}
-	}
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo) {
+        if (atual == null) {
+            return novoNo;
+        } else if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        } else {
+            atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+        }
+        return atual;
+    }
 
-	public void exibirPosOrdem(){
-		System.out.println("\n exibir em PosOrdem");
-		exibirPosOrdem(this.raiz);
-	}
+    public void exibirInOrdem() {
+        System.out.println("\n exibir em InOrdem");
+        exibirInOrdem(this.raiz);
+    }
 
-	private void exibirPosOrdem(BinNo<T> atual) {
-		if (atual != null) {
-			exibirPosOrdem(atual.getNoEsq());
-			exibirPosOrdem(atual.getNoDir());
-			System.out.println(atual.getConteudo() + ", ");
-		}
-	}
+    private void exibirInOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            exibirInOrdem(atual.getNoEsq());
+            System.out.println(atual.getConteudo() + ", ");
+            exibirInOrdem(atual.getNoDir());
+        }
+    }
 
-	public void exibirPreOrdem(){
-		System.out.println("\n exibir em PreOrdem");
-		exibirPreOrdem(this.raiz);
-	}
+    public void exibirPosOrdem() {
+        System.out.println("\n exibir em PosOrdem");
+        exibirPosOrdem(this.raiz);
+    }
 
-	private void exibirPreOrdem(BinNo<T> atual) {
-		if (atual != null) {
-			System.out.println(atual.getConteudo() + ", ");
-			exibirPreOrdem(atual.getNoEsq());
-			exibirPreOrdem(atual.getNoDir());
-		}
-	}
+    private void exibirPosOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            exibirPosOrdem(atual.getNoEsq());
+            exibirPosOrdem(atual.getNoDir());
+            System.out.println(atual.getConteudo() + ", ");
+        }
+    }
+
+    public void exibirPreOrdem() {
+        System.out.println("\n exibir em PreOrdem");
+        exibirPreOrdem(this.raiz);
+    }
+
+    private void exibirPreOrdem(BinNo<T> atual) {
+        if (atual != null) {
+            System.out.println(atual.getConteudo() + ", ");
+            exibirPreOrdem(atual.getNoEsq());
+            exibirPreOrdem(atual.getNoDir());
+        }
+    }
+
+    public void remover(T conteudo) {
+        try {
+            BinNo<T> atual = this.raiz;
+            BinNo<T> pai = null;
+            BinNo<T> filho = null;
+            BinNo<T> temp = null;
+
+            while (atual != null && !atual.getConteudo().equals(conteudo)){
+                pai = atual;
+                if (conteudo.compareTo(atual.getConteudo()) < 0){
+                    atual = atual.getNoEsq();
+                } else{
+                    atual = atual.getNoDir();
+                }
+            }
+
+            if(atual == null){
+                System.out.println("Conteudo não encontrado. Bloco Try");
+            }
+        } catch (NullPointerException erro) {
+            System.out.println("Conteudo não encontrado. Bloco Catch");
+        }
+    }
 }
